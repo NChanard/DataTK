61e749253d1795896aaf32e5e44ac52e1b1765a2
NChanard <nchanard@univ-tlse3.fr>
2022-09-16 13:09:06 +0200

    Add BindFillRows function

--- R/TransposeList.r
+++ R/TransposeList.r
@@ -7,11 +7,11 @@
 #' @examples
 #' my_lst <- list(
 #'     first=list("A1","B1","C1"),
-#'     second=list("A2","B2")
+#'     second=list("A2","B2"),
+#'     third=list(NULL,"B3")
 #' )
 #' TransposeList(my_lst)
 TransposeList <- function(var.nlst){
-    listName.chr <- names(var.nlst)
     var.nlst %>%
         lapply(length) %>%
         unlist %>%
@@ -20,10 +20,14 @@ TransposeList <- function(var.nlst){
         lapply(function(newLst.ndx){
             new.lst <- var.nlst %>% 
                 lapply(function(ele.lst){
-                    ifelse(length(ele.lst) >= newLst.ndx, yes=ele.lst[[newLst.ndx]], no=NA)
+                    if(length(ele.lst) >= newLst.ndx){
+                        return(ele.lst[[newLst.ndx]])
+                    }
+                    else{
+                        return(NA)
+                    }
                     }) %>%
-                unlist %>%
-                magrittr::set_names(listName.chr)
+                unlist
             return(new.lst[!is.na(new.lst)])
         })
 }
